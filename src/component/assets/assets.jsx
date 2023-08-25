e7b9c50ada264af4b848d7bde010953a1ff7dff9
Alison Colao <alison.siwek@gmail.com>
2023-08-25 14:28:19 -0700

    updated icon and styling

--- src/component/assets/assets.jsx
+++ src/component/assets/assets.jsx
@@ -2,3 +2,4 @@ export { default as passwordJavaScript } from "./password.png";
 export { default as feedMe } from "./feedme.png";
 export { default as dayTripper } from "./daytrip.png";
 export { default as workdayScheduler } from "./planner.jpg";
+export { default as logo } from "./cherry.png";
