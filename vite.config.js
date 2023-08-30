c13475f3c5f388d6079d8764ce434f20fc98ef8e
Alison Colao <alison.siwek@gmail.com>
2023-08-30 10:50:11 -0700

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-const PORT = process.env.PORT || 3001;
+//const PORT = process.env.PORT || 3001;
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
