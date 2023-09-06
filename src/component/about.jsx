27d629556c73e5e13da5b05533a8fc5ca7e2ea59
Alison Colao <alison.siwek@gmail.com>
2023-09-06 12:52:30 -0700

    about page styling

--- src/component/about.jsx
+++ src/component/about.jsx
@@ -4,8 +4,8 @@ export default function About() {
         <h1>Hello there!</h1>
         <div className="about">
           <p>
-            I'm Alison Colao, a full stack web developer with a flair for turning
-            ideas into interactive realities. With a foundation in both
+            I'm Alison Colao, a full stack web developer with a flair for
+            turning ideas into interactive realities. With a foundation in both
             front-end finesse and back-end wizardry, I craft seamless,
             user-centric websites and applications that marry functionality with
             aesthetics.
